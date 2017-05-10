08ba385bc5a0bc82120360e1413e780f9f01d11e
aniketshukla <asr563up@gmail.com>
2017-05-10 18:49:09 +0530

    env changes

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 process.env['MONGOLAB_URI']='mongodb://heroku_prmmpl5w:9mr5vj89861592giv0mkbcf511@ds153710.mlab.com:53710/heroku_prmmpl5w';
 process.env['MONGOLAB_URI_TESTING']='mongodb://heroku_prmmpl5w:9mr5vj89861592giv0mkbcf511@ds153710.mlab.com:53710/heroku_prmmpl5w';
-process.env['port']='8080';
-process.env['testing_port']='8080';
+process.env['port']='5000';
+process.env['testing_port']='5000';
