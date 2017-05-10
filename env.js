1e2191338df4f43c595c655761a84e101a73f627
aniketshukla <asr563up@gmail.com>
2017-05-10 19:28:58 +0530

    added env for port no

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 process.env['MONGOLAB_URI']='mongodb://heroku_prmmpl5w:9mr5vj89861592giv0mkbcf511@ds153710.mlab.com:53710/heroku_prmmpl5w';
 process.env['MONGOLAB_URI_TESTING']='mongodb://heroku_prmmpl5w:9mr5vj89861592giv0mkbcf511@ds153710.mlab.com:53710/heroku_prmmpl5w';
-process.env['port']='5000';
-process.env['testing_port']='5000';
+process.env['port']=process.env['PORT'];
+process.env['testing_port']=process.env['PORT'];
